0ba10c4937a244d6107193242af298dad6395deb
marecica2 <marecica2@gmail.com>
2020-03-25 12:03:52 +0100

    DWH-17: Change migrator to service instead of cli

--- app-migrator/src/main/java/org/bmsource/dwh/migrator/MigratorApplication.java
+++ app-migrator/src/main/java/org/bmsource/dwh/migrator/MigratorApplication.java
@@ -1,33 +1,54 @@
 package org.bmsource.dwh.migrator;
 
+import org.bmsource.dwh.common.portal.PortalConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
-@EnableTransactionManagement
-public class MigratorApplication implements CommandLineRunner {
+import javax.annotation.PostConstruct;
 
-    @Autowired
-    Migrator migrator;
+@SpringBootApplication(
+    scanBasePackageClasses = {
+        MigratorApplication.class,
+    },
+    exclude = {
+        RepositoryRestMvcAutoConfiguration.class
+    }
+)
+@EnableJpaRepositories(
+    basePackageClasses = {
+        MigratorApplication.class,
+        PortalConfiguration.class,
+    }
+)
+@EntityScan(basePackageClasses = {
+    MigratorApplication.class,
+    PortalConfiguration.class,
+})
+@EnableTransactionManagement
+public class MigratorApplication {
 
     private static Logger logger = LoggerFactory
         .getLogger(MigratorApplication.class);
 
-    public static void main(String[] args) {
+    @Autowired
+    Migrator migrator;
+
+    @PostConstruct
+    public void init() {
         logger.info("Migrating schemas");
-        SpringApplication.run(MigratorApplication.class, args);
+        migrator.migrate();
         logger.info("Migration completed");
     }
 
-    @Override
-    public void run(String... args) {
-        migrator.migrate();
-        System.exit(0);
+    public static void main(String[] args) {
+        SpringApplication.run(MigratorApplication.class, args);
     }
 }
